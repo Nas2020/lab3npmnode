8cb334b07f4705548a227b7ed994b08ed948c7e5
nlmuasys <nlmuasys@gmail.com>
2020-03-29 01:47:26 -0400

    Lab5 added

---
+++ Lab_5/server.js
@@ -0,0 +1,54 @@
+var http = require('http')
+  , url = require('url')
+  , fs = require('fs')
+  , io = require('socket.io')
+  , server;
+
+
+server = http.createServer(function(req, res){
+  
+  var path = url.parse(req.url).pathname;
+  switch (path){
+    case '/':
+      fs.readFile(__dirname + '/index.html', function(err, data){
+          if (err) return send404(res);
+          res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
+          res.write(data, 'utf8');
+          res.end();
+      });
+      break;
+
+    default: send404(res);
+  }
+}),
+
+send404 = function(res){
+  res.writeHead(404);
+  res.write('404');
+  res.end();
+};
+
+server.listen(8080);
+
+
+// socket.io, setup
+var io = io.listen(server);
+
+io.on('connection', function(client){
+
+
+  // now we have a client object!
+console.log("connection accepted.");
+
+// event listeners
+  client.on('message', function(message){
+       console.log(`Recieved message: ${message} - from client`);
+       client.emit('msgreceived');
+});
+
+client.on("disconnect", function(){
+   console.log("Disconnected...");
+});
+
+});
+// setup socket listeners here
