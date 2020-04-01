cafdee8f9e5a8072468c9615e4f20b384b51270c
nlmuasys <nlmuasys@gmail.com>
2020-04-01 05:20:11 -0400

    Lab 6

--- Lab_2/difference.js
+++ Lab_2/difference.js
@@ -1,8 +1,12 @@
 
-function difference(n)
- {
-    if (n <= 13)
-        return 13 - n;
-    else
-        return (n - 13) * 2;
- }
+// function difference(n)
+//  {
+//     if (n <= 13)
+//         return 13 - n;
+//     else
+//         return (n - 13) * 2;
+//  }
+
+//ES6 
+ const difference = (n) => (n <= 13) ? (13 - n) : (n - 13) * 2;
+ console.log(difference(10));
